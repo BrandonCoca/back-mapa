01913c8844c7be244d52221568673fdd17a6e340
Brandon Coca <brand0nakcher@gmail.com>
2025-12-10 10:40:06 -0400

    Subir 4

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   // Utiliza la variable de entorno DATABASE_URL configurada en Render
   connectionString: process.env.DATABASE_URL, 
-  
+  host: 'db.albllifgogsxzvoijjfl.supabase.co', // Vuelve a especificar el host para asegurar que se aplique 'family'
+  family: 4,
   // SSL requerido para Supabase/nube
   ssl: {
     rejectUnauthorized: false 
