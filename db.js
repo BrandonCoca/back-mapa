3364cf524f5fc7f57c94b3c0862ec17655512d9c
Brandon Coca <brand0nakcher@gmail.com>
2025-12-10 11:04:42 -0400

    Fix IPv4 connection for Render

--- db.js
+++ db.js
@@ -1,17 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  // Parámetros explícitos extraídos de la URL
-  user: 'postgres', // Usuario predeterminado de Supabase
-  password: process.env.DB_PASSWORD, // Usa la nueva variable de entorno
-  host: process.env.DB_HOST || 'db.albllifgogsxzvoijjfl.supabase.co', // Usa la variable DB_HOST o el valor fijo
-  port: 5432,
-  database: 'postgres',
+  connectionString: process.env.DATABASE_URL,
   
-  // *** SOLUCIÓN CLAVE ***
-  family: 4, 
+  // Forzar IPv4 para evitar error ENETUNREACH en Render
+  family: 4,
   
-  // SSL requerido
+  // SSL requerido para Supabase
   ssl: {
     rejectUnauthorized: false 
   }
