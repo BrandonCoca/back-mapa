e8fd8debc11680144baa38f66b7526354fe1dd18
Brandon Coca <brand0nakcher@gmail.com>
2025-12-10 10:28:15 -0400

    Subir 3

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+  // Utiliza la variable de entorno DATABASE_URL configurada en Render
+  connectionString: process.env.DATABASE_URL, 
+  
+  // SSL requerido para Supabase/nube
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 module.exports = pool;
